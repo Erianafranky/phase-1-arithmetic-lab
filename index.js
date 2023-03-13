a207d5ca66c877e8788f9daeaf8debcad560beaa
DorothyAriana <arianadottie@gmail.com>
2023-03-13 23:15:19 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 25;
+let num4= 7;
+let mod = Math.ceil(num3 / num4);
+
+
+let max = Math.max(5, 10, 15,20);
